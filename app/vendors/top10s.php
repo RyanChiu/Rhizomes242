6b742376dcc2f44a84602797ef2440ce09bab19f
ray <RyanChiu@hotmail.com>
2016-08-30 14:44:56 +0800

    hide old sales from top10

--- app/vendors/top10s.php
+++ app/vendors/top10s.php
@@ -25,6 +25,7 @@ $_sql_ =
 		sum(sales_number - chargebacks) as sales 
 	FROM stats, accounts, agents, companies   
 	WHERE %s
+		and stats.siteid not in (2, 7, 8, 11)
 		and stats.agentid = accounts.id and agents.id = stats.agentid 
 		and agents.companyid = companies.id 
 		AND agentid > 0 
