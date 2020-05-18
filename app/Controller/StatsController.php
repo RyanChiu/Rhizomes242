036db373c813170d2dc7a5b6d6ccfbf245fe82b7
ray <ryan.chiu@hotmail.com>
2020-05-18 03:10:42 +0000

    make stats link in menu item defaultly "by date"

--- app/Controller/StatsController.php
+++ app/Controller/StatsController.php
@@ -507,7 +507,7 @@ class StatsController extends AppController {
 			if (!(array_key_exists('page', $this->passedArgs) || array_key_exists('sort', $this->passedArgs))) {
 				//if it's not paginating, then it should be drilling down
 				if ($this->Session->check('conditions_stats')
-					|| (array_key_exists('clear', $this->passedArgs) && $this->passedArgs['clear'] == -2)) {
+					/*|| (array_key_exists('clear', $this->passedArgs) && $this->passedArgs['clear'] == -2)*/) {
 					
 					if (array_key_exists('clear', $this->passedArgs) && $this->passedArgs['clear'] == -2) {
 						$tmp_periods = array_keys($periods);
